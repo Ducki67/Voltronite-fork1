84e1cbf48b0c31287c48e1e4c2c5c643d927a015
Ducki67 <sabel200901@gmail.com>
2025-09-26 14:49:10 +0200

    `Start.bat` for starting the backend
    This runs the command in BUN to run the backend
    
    ALSO u can use the zipped file or edit it to make it for other backends too:))

---
+++ Start backend.bat
@@ -0,0 +1,61 @@
+@echo off
+chcp 65001 >nul
+color 3
+Title= Starting Backend!
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠙
+Title= Starting Backend ⠋
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠋
+Title= Starting Backend ⠋
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠹
+Title= Starting Backend ⠹
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠸
+Title= Starting Backend ⠸
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠼
+Title= Starting Backend ⠼
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠴
+Title= Starting Backend ⠴
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠦
+Title= Starting Backend ⠦
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠧
+Title= Starting Backend ⠧
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠇
+Title= Starting Backend ⠇
+ping localhost -n 2 >nul
+cls
+echo.
+echo Starting ⠏
+ping localhost -n 2 >nul
+Title=⚡▶ Backend is Up!! ◀⚡
+cls
+echo.
+echo 🚀⚡▶ Voltranite is Runing! ◀⚡🚀
+echo Source here: https://github.com/RazerFrFr/Voltronite
+echo Credits: RazerFrFR
+bun run src/index.ts
