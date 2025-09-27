8db691025cc0ebfd4c4332a25d3cfe0dcd72c284
Ducki67 <sabel200901@gmail.com>
2025-09-27 23:07:01 +0200

    Update Start backend.bat

--- Start backend.bat
+++ Start backend.bat
@@ -12,46 +12,46 @@ cls
 echo.
 echo Starting ⠋
 Title= Starting Backend ⠋
-ping localhost -n 2 >nul
+ping localhost -n 1.5 >nul
 cls
 echo.
 echo Starting ⠹
 Title= Starting Backend ⠹
-ping localhost -n 2 >nul
+ping localhost -n 1.5 >nul
 cls
 echo.
 echo Starting ⠸
 Title= Starting Backend ⠸
-ping localhost -n 2 >nul
+ping localhost -n 1.5 >nul
 cls
 echo.
 echo Starting ⠼
 Title= Starting Backend ⠼
-ping localhost -n 2 >nul
+ping localhost -n 1.5 >nul
 cls
 echo.
 echo Starting ⠴
 Title= Starting Backend ⠴
-ping localhost -n 2 >nul
+ping localhost -n 1.5 >nul
 cls
 echo.
 echo Starting ⠦
 Title= Starting Backend ⠦
-ping localhost -n 2 >nul
+ping localhost -n 1.5 >nul
 cls
 echo.
 echo Starting ⠧
 Title= Starting Backend ⠧
-ping localhost -n 2 >nul
+ping localhost -n 1.3 >nul
 cls
 echo.
 echo Starting ⠇
 Title= Starting Backend ⠇
-ping localhost -n 2 >nul
+ping localhost -n 1.3 >nul
 cls
 echo.
 echo Starting ⠏
-ping localhost -n 2 >nul
+ping localhost -n 1.3 >nul
 Title=⚡▶ Backend is Up!! ◀⚡
 cls
 echo.
